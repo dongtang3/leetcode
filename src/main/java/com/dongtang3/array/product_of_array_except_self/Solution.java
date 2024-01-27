f918ea0a2b11e354dc9abc1b259b7b8e9fd20d79
DongTang <eliuinz@gmail.com>
2024-01-26 19:52:55 -0500

    init

---
+++ src/main/java/com/dongtang3/array/product_of_array_except_self/Solution.java
@@ -0,0 +1,20 @@
+package com.dongtang3.array.product_of_array_except_self;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        for(int i = 0; i< n;i++){
+            int pro = 1;
+            for(int j = 0; j < n;j++){
+                if(i == j) {
+                    continue;
+
+                }
+                pro *= nums[j];
+            }
+            ans[i] = pro;
+        }
+        return ans;
+    }
+}
